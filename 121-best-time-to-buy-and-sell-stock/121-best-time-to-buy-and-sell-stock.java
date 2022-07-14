f2600c4156d026a96ebec2cba669390171749fac
Rushikesh Mali <91449271+RushikeshMali05@users.noreply.github.com>
2022-07-14 11:40:47 +0530

    Time: 3 ms (67.17%), Space: 76.7 MB (75.80%) - LeetHub

---
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int start =prices[0];
+        int maxProfit =0;
+        
+        for(int i=1;i<prices.length;i++){
+            int cost= prices[i];
+           int profit = cost-start;
+            maxProfit = Math.max(profit,maxProfit);
+            start = Math.min(start,cost);
+        }
+        
+        return maxProfit;
+        
+    }
+}
