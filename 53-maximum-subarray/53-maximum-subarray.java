2bed67b97ecfce52546411e96a37e6b01bb97d0e
Rushikesh Mali <91449271+RushikeshMali05@users.noreply.github.com>
2022-08-16 13:34:37 +0530

    Time: 2 ms (74.62%), Space: 75.5 MB (23.71%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.java
+++ 53-maximum-subarray/53-maximum-subarray.java
@@ -1,24 +1,31 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-     int curSum =nums[0];
-        int maxSum =nums[0];
+   
+        int curSum = nums[0];
+        int maxSum = nums[0];
         
         for(int i=1;i<nums.length;i++){
-            
+                   
             if(curSum + nums[i] < nums[i]){
                 
                 curSum = nums[i];
-            }
             
-            else{
+            } else {
                 
-                curSum = curSum + nums[i];
+                curSum += nums[i];
+                
             }
             
-            if(curSum > maxSum ){
-                maxSum = curSum;
+            if(maxSum<curSum){
+
+            maxSum = curSum;
+                
             }
+            
+        
+        
         }
-        return maxSum;
+                    return maxSum;
+
 }
 }
