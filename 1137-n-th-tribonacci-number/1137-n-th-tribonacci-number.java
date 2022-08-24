f079d63ee495cefe9cc8dde3e1fe410da9655d5d
Rushikesh Mali <91449271+RushikeshMali05@users.noreply.github.com>
2022-08-24 21:36:13 +0530

    Time: 0 ms (100.00%), Space: 40.5 MB (72.93%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -1,23 +1,16 @@
 class Solution {
+    int[] dp=new int[38];
     public int tribonacci(int n) {
-        if(n==0 || n==1){
-            return n;
-        }
-        int[] dp=new int[n+1];
-        Arrays.fill(dp,-1);
-        return tib(n,dp);
-    }
-    public int tib(int n,int[] dp){
         if(n<=0){
             return 0;
         }
-        if(n==1 || n==2){
+        if(n==1 ||  n==2){
             return 1;
         }
-        if(dp[n]!=-1){
+        if(dp[n]!=0){
             return dp[n];
         }
-        dp[n]=tib(n-1,dp)+tib(n-2,dp)+tib(n-3,dp);
+        dp[n]=tribonacci(n-1)+tribonacci(n-2)+tribonacci(n-3);
         return dp[n];
     }
 }
