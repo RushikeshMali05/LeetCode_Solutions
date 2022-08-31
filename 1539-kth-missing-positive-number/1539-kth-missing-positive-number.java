82fb9af98a61d8ac89ad61d507595d69ebfb9238
Rushikesh Mali <91449271+RushikeshMali05@users.noreply.github.com>
2022-08-31 23:57:04 +0530

    Time: 0 ms (100.00%), Space: 43.4 MB (39.95%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
@@ -1,47 +1,19 @@
 class Solution {
-    public int findKthPositive(int[] arr, int k) {
-       int j=1;
-        int i=0;
-        ArrayList<Integer> list = new ArrayList<Integer>();
-        System.out.println(Arrays.toString(arr));
-        while(i<arr.length){
-            System.out.println(j+" "+arr[i]+" "+(j==arr[i]));
-           if(j == arr[i]){
-              j++;
-               i++;
-                
-           } else {
- list.add(j);
-               System.out.println(j);
-               j++;
-                          }
-            
-       }
-        System.out.println(list);
-        if(list.size()<k){
-            return arr[arr.length-1]+k-list.size();
-        }
-        return list.get(k-1);
-    }
-    
-    
-//     public boolean binarySearch(int[] arr, int target){
-//          int start =0;
-//         int end = arr.length-1;
-        
-//         while(start<=end){
-//             int mid = start + (end-start)/2;
-            
-//             if(arr[mid] == target){
-//                 return true;
-//             }
-            
-            
-//             if(arr[mid] < target){
-//                 start + mid+1;
-//             } else {
-//                 end = mid-1;
-//             }
-//         }
-//     }
+public int findKthPositive(int[] arr, int k) {
+int s =0;
+int e = arr.length -1;
+while(s<=e)
+{
+int m = s+(e-s)/2;
+if(arr[m]-(m+1)<k)//answer lies on the second half of array
+{
+s = m+1;
+}
+else
+{
+e = m-1;
+}
+}
+return s+k;
+}
 }
