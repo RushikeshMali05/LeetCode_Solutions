28a9e9d22ab791a629aecb427b7780098c4824b8
Rushikesh Mali <91449271+RushikeshMali05@users.noreply.github.com>
2022-09-09 21:27:41 +0530

    Time: 3 ms (56.52%), Space: 40.7 MB (89.40%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.java
+++ 389-find-the-difference/389-find-the-difference.java
@@ -1,14 +1,23 @@
 class Solution {
     public char findTheDifference(String s, String t) {
-        char[] arr1 = s.toCharArray();
+       char[] arr1 = s.toCharArray();
         char[] arr2 = t.toCharArray();
+
         Arrays.sort(arr1);
         Arrays.sort(arr2);
-        for(int i = 0 ; i < arr1.length ; i++){
-            if(arr1[i] != arr2[i]){
+
+        for(int i=0;i<arr1.length;i++){
+            
+            if(arr1[i]!=arr2[i]){
                 return arr2[i];
             }
+            
         }
-        return arr2[arr2.length -1];
+
+        return arr2[arr2.length-1];
     }
 }
+
+
+
+     
