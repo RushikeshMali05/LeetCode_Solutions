0758e3e9ed15912f635c72fdce1d647817f3651b
Rushikesh Mali <91449271+RushikeshMali05@users.noreply.github.com>
2022-09-16 17:32:30 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (34.04%) - LeetHub

--- 367-valid-perfect-square/367-valid-perfect-square.java
+++ 367-valid-perfect-square/367-valid-perfect-square.java
@@ -1,29 +1,24 @@
 class Solution {
     public boolean isPerfectSquare(int num) {
-        if(num == 0){
-            return true;
-        }
+        
+        if(num == 0 || num ==1) return true;
         
         long start =1;
         long end = num;
-        long ans = -1;
         
         while(start<=end){
-            long mid = start+(end-start)/2;
             
-            if((mid*mid)==num){
+            long mid = start + (end - start)/2;
+            
+            if((mid*mid) == num){
                 return true;
+            } else if(num<(mid*mid)){
+                end = mid-1;
+            } else {
+                start = mid+1;
             }
-            else if (num>(mid*mid)){
-                                start = mid+1;
-
-            }
-            else{
-                            end = mid-1;                     
-
-            }
-        }
+        }return false;
         
-        return false;
+        
     }
 }
