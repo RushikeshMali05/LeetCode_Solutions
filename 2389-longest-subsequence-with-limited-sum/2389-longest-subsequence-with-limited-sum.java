32c9f65bba4dbc41e3da87b4c605bba18755c27d
Rushikesh Mali <91449271+RushikeshMali05@users.noreply.github.com>
2022-09-16 21:15:59 +0530

    Time: 8 ms (71.40%), Space: 49.1 MB (38.51%) - LeetHub

--- 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.java
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.java
@@ -1,19 +1,37 @@
 class Solution {
-    public int[] answerQueries(int[] nums, int[] queries) {
-       Arrays.sort(nums);
-    for(int i = 0; i < queries.length; i++) {
-        int curr = 0;
-        int count = 0;
-        for(int j = 0; j < nums.length; j++) {
-            curr += nums[j];
-            if(curr <= queries[i]) {
-                count++;
-            }
-            else 
-                curr -= nums[j];
+public int[] answerQueries(int[] nums, int[] queries) {
+    Arrays.sort(nums);
+    int[] prefixArray = new int[nums.length];
+    prefixArray[0] = nums[0];
+    for(int i = 1; i < nums.length; i++){
+        prefixArray[i] = prefixArray[i-1] + nums[i];
+    }
+    int[] answer = new int[queries.length];
+    for(int i = 0; i < queries.length; i++){
+        int num = queries[i];
+        int index = binarySearch(prefixArray, num);
+        answer[i] = index + 1;
+    }
+    return answer;
+}
+
+private int binarySearch(int[] array, int sum){
+    int start = 0;
+    int end = array.length - 1;
+    
+    int index = -1;
+    
+    while(start <= end){
+        int mid = start + (end - start)/2;
+        int num = array[mid];
+        
+        if(num <= sum){
+            index = Math.max(index, mid);
+            start = mid + 1;
+        }else if(num > sum){
+            end = mid - 1;
         }
-        queries[i] = count;
     }
-    return queries; 
-    }
+    return index;
+}
 }
