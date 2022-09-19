0c19347d88b02956f128ad292b877725a8ba4773
Rushikesh Mali <91449271+RushikeshMali05@users.noreply.github.com>
2022-09-19 22:37:24 +0530

    Time: 3 ms (74.98%), Space: 41.6 MB (41.91%) - LeetHub

--- 441-arranging-coins/441-arranging-coins.java
+++ 441-arranging-coins/441-arranging-coins.java
@@ -1,10 +1,22 @@
 class Solution {
     public int arrangeCoins(int n) {
-        int count = 0;
-        for(int i = 1; i<=n; i++){
-            n = n - i;
-            count++;
+        
+        long start = 0, end = n;
+        long mid, temp;
+        
+        while (start <= end) {
+            
+            mid = start + (end - start) / 2;
+            temp = mid * (mid + 1) / 2;
+
+            if (temp == n)
+                return (int)mid;
+            else if (temp < n)
+                start = mid + 1;
+            else
+                end = mid - 1;
         }
-       return count;
+        
+        return (int)end;
     }
 }
