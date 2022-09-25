f124e993dbc6ed2c64c88850b6090d6101500b8b
Rushikesh Mali <91449271+RushikeshMali05@users.noreply.github.com>
2022-09-25 13:48:51 +0530

    Time: 5 ms (28.48%), Space: 43.5 MB (48.64%) - LeetHub

---
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean checkIfExist(int[] nums) {
+     
+        Arrays.sort(nums);
+        
+        for(int i=0;i<nums.length;i++){
+            
+            int start=0;
+            int end = nums.length-1;
+            int target = 2*nums[i];
+            
+            while(start <= end){
+                
+                int mid = start + (end - start)/2;
+                
+                if(nums[mid] == target && mid!=i){
+                    return true;
+                } 
+                if(nums[mid] < target){
+                    start = mid +1;
+                } else {
+                    end = mid -1;
+                }
+                
+                
+            }
+            
+        }
+        return false;
+        
+    }
+}
