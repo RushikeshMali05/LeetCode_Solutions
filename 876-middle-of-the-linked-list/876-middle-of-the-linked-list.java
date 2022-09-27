e582a6dab7196899e25b6c40c34cf6eacbd83bcf
Rushikesh Mali <91449271+RushikeshMali05@users.noreply.github.com>
2022-09-27 23:11:14 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (54.32%) - LeetHub

--- 876-middle-of-the-linked-list/876-middle-of-the-linked-list.java
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.java
@@ -9,19 +9,31 @@
  * }
  */
 class Solution {
-public ListNode middleNode(ListNode head) {
-    if(head.next==null || head.next.next==null)
-        return head.next==null?head:head.next;
-    int size=0,mid=0;
-    ListNode curr=head;
-    while(curr!=null){
-        size++;
-        curr=curr.next;
+    public ListNode middleNode(ListNode head) {
+        
+        if(head.next == null || head.next.next == null){
+            
+            return head.next == null ? head : head.next;
+        }
+        
+        int size =0;
+        
+        ListNode curr = head;
+        
+        while(curr!=null){
+            size++;
+            curr = curr.next;
+            
+        }
+        
+        int mid = size/2 + 1;
+        
+        for(int i=1;i<mid;i++){
+
+            head = head.next;
+        
+        }
+        return head;
     }
-    mid=size/2+1;
-    for(int i=1;i<mid;i++){
-        head=head.next;
-    }
-    return head;
-}
+    
 }
