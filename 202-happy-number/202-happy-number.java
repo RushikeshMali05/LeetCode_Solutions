5953ecf3cad5202c4ece97e871a9b12e95b24d1e
Rushikesh Mali <91449271+RushikeshMali05@users.noreply.github.com>
2022-09-29 23:19:33 +0530

    Time: 4 ms (24.31%), Space: 41 MB (65.23%) - LeetHub

--- 202-happy-number/202-happy-number.java
+++ 202-happy-number/202-happy-number.java
@@ -1,30 +1,34 @@
 class Solution {
     public boolean isHappy(int n) {
-     
+        
         HashSet<Integer> set = new HashSet<>();
-       
-        while(!set.contains(n)){
+        
+            while(!set.contains(n)){
+
+                set.add(n);
+                n = sumOfSq(n);
+                
+                if(n==1) return true;
+                
             
-            set.add(n);
-            n =sumOfSq(n);
-            
-            if(n==1) return true;
-            
-        }
+            }
+        
         return false;
         
     }
     
-    public int  sumOfSq(int i){
+    public int sumOfSq(int i){
         
-         int rem =0;
+        int rem =0;
         int sum=0;
-        while(i>0){
-        rem = i%10;
-        i=i/10;
-       sum = sum + (rem*rem);
         
+        while(i>0){
+            
+            rem = i%10;
+            i = i/10;
+            sum = sum+(rem*rem);
+            
+        }
+        return sum;
     }
-    return sum;
-}
 }
